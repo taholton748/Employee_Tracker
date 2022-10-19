8bef70b26ef3d44e31810ebd81e77fb416c9460c
Medici <107539009+taholton748@users.noreply.github.com>
2022-10-18 19:48:02 -0600

    added values to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,24 +4,36 @@ INSERT INTO department
     (name)
 VALUES
     ('Sales'),
-    ('IT'),
-    ('Legal'),
-    ('Engineering'),
-    ('Finance');
+    ('Resources'),
+    ('Finance'),
+    ('Shipping');
 
 INSERT INTO role 
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 75000, 1),
-    ('IT Support', 1000000, 2),
-    ('Lawyer', 30000, 3),
-    ('Engineer', 150000, 4),
-    ('Accountant', 51000, 5);
+    ('Branch Co-Manager', 160000, 1),
+    ('Sales Associate', 90000, 1),
+    ('Customer Service', 40000, 2),
+    ('Supplier Relations', 55000, 2),
+    ('Human Resources', 60000, 2),
+    ('Head of Accounting', 80000, 3),
+    ('Accountant', 65000, 3),
+    ('Warehouse Associate', 45000, 4);
 
 INSERT INTO employee 
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Tyler', 'Holton', 1, NULL),
-    ('Tyler', 'Holton', 4, NULL),
-    ('Tyler', 'Holton', 5, NULL),
-    ('Tyler', 'Holton', 3, NULL);
+    ('Jim', 'Halpert', 1, NULL),
+    ('Dwight', 'Schrute', 1, NULL),
+    ('Pam', 'Beesly', 2, NULL),
+    ('Angela', 'Martin', 6, NULL),
+    ('Kelly', 'Kapoor', 3, NULL),
+    ('Andy', 'Bernard', 2, NULL),
+    ('Ryan', 'Howard', 2, NULL),
+    ('Toby', 'Flenderson', 5, NULL),
+    ('Kevin', 'Malone', 7, NULL),
+    ('Stanley', 'Hudson', 2, NULL),
+    ('Oscar', 'Martinez', 7, NULL),
+    ('Meredith', 'Palmer', 4, NULL),
+    ('Daryll', 'Philbin', 8, NULL),
+    ('Phyllis', 'Vance', 2, NULL);
